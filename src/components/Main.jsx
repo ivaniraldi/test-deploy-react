dda6e9f2dfdc8c1394fee24b481ba30f3914003c
Ivan Iraldi, SykoTech <iraldiban@gmail.com>
2026-04-15 21:44:19 -0300

    Render

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,7 @@ function Main (){
     return(
         <main>
             <h1>Bienvenidos a mi aplicación</h1>
+            <h2>Estoy desplegando en Render</h2>
             <p>Este es el contenido principal de la página.</p>
         </main>
     )
